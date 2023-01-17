1a819e510c1687c6ecf206d9518d9de5c40527fb
Piotr <89880037+Piotr1178@users.noreply.github.com>
2023-01-17 18:42:22 +0000

    modified:   index.js 	new file:   public/css/home.css 	new file:   public/css/login.css 	new file:   views/index.ejs 	new file:   views/login.ejs

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//
+const fetch = require('node-fetch')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const wait = ms => new Promise(res => setTimeout(res, ms))
+const app = express()
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use(cookieParser());
+
+app.use(express.json())
+
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('index.ejs')
+})
+
+app.get('/login', function (req, res) {
+    res.render('login.ejs')
+})
+
+app.listen(3000)
